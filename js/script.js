8820d640f0f094fb7b4a3417d0d92cae886233b8
AndreiCiobanasu <a_ciobanasu@yahoo.fr>
2017-11-27 10:38:36 +0100

    Création Pop-up cookies

--- js/script.js
+++ js/script.js
@@ -1,3 +1,21 @@
+/* POP-UP COOKIES */
+window.addEventListener("load", function(){
+window.cookieconsent.initialise({
+"palette": {
+"popup": {
+"background": "#64386b",
+"text": "#ffcdfd"
+},
+"button": {
+"background": "transparent",
+"text": "#f8a8ff",
+"border": "#f8a8ff"
+}
+},
+"type": "opt-in"
+})});
+
+
 
  /* BOUTTON LOGIN */
 var login = document.getElementById("login");
@@ -7,9 +25,10 @@ var formulaire = document.getElementById("formulaire");
 function seeLogin() {
 	login.style.display = "flex";
 	formulaire.style.display = "flex";
-}
+};
 
 function hiddenLogin() {
 	login.style.display = "none";
 	formulaire.style.display = "none";
 }
+
