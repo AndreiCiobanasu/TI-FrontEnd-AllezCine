c1571233afde9b2ed61d791d35a5634bc56446ba
AndreiCiobanasu <a_ciobanasu@yahoo.fr>
2017-11-27 09:18:25 +0100

    Création de la navbar

---
+++ js/script.js
@@ -0,0 +1,15 @@
+
+ /* BOUTTON LOGIN */
+var login = document.getElementById("login");
+var formulaire = document.getElementById("formulaire");
+
+
+function seeLogin() {
+	login.style.display = "flex";
+	formulaire.style.display = "flex";
+}
+
+function hiddenLogin() {
+	login.style.display = "none";
+	formulaire.style.display = "none";
+}
