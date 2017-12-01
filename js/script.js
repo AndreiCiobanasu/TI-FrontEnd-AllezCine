f6446faa02befd4459be31abf7109ebb42c21cee
AndreiCiobanasu <a_ciobanasu@yahoo.fr>
2017-12-01 11:32:38 +0100

    Ajout de la Séction Films

--- js/script.js
+++ js/script.js
@@ -82,6 +82,33 @@ $(document).ready(function() {
 
 
 
+/*////////////////////////////////////////////////////////////////////
+					MOVIES
+////////////////////////////////////////////////////////////////////*/
+//Pour que la Vidéo s'arrette quand on ferme le Modal
+$(document).ready(function(){
+/* Get iframe src attribute value i.e. YouTube video url
+and store it in a variable */
+var url = $("#cartoonVideo").attr('src');
+
+/* Remove iframe src attribute on page load to
+prevent autoplay in background */
+$("#cartoonVideo").attr('src', '');
+
+/* Assign the initially stored url back to the iframe src
+attribute when modal is displayed */
+$("#myModal").on('shown.bs.modal', function(){
+    $("#cartoonVideo").attr('src', url);
+});
+
+/* Assign empty url value to the iframe src attribute when
+modal hide, which stop the video playing */
+$("#myModal").on('hide.bs.modal', function(){
+    $("#cartoonVideo").attr('src', '');
+});
+});
+
+
 
 
 
